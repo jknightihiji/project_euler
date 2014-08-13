29a16fd3c341c7f6939f523710d3bb71b15f4b18
jknightihiji <jknight@ihiji.com>
2014-08-12 23:52:02 -0500

    first solution to problem 5 - brute force
    solved

---
+++ problem5/solution.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+i = 1 
+while True :
+    valid = 0 
+    for divisor in range(20,0,-1) :
+        if i % divisor == 0 :
+            valid = 1 
+        else :
+            valid = 0
+            break
+    if valid == 1 :
+        print "answer: "+str(i)
+        break
+    if i % 100000 == 0 :
+       print i 
+    i+=1 
